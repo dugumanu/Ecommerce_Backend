e0f0fca07363f62731cd1e71dcc8ad0ef7aaa7eb
rahulkumarv269 <rahulkumarv269@gmail.com>
2025-01-16 22:49:19 +0530

    updated

--- route/Order.js
+++ route/Order.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { auth, isAdminOrSeller, isAdmin } = require("../middleware/auth");
-const { buyOrder } = require("../controller/Order");
+const { buyOrder, myOrder, sellerOrder, orderStatus } = require("../controller/Order");
 const router = express.Router();
 router.post("/buyorder",  auth, buyOrder );
+router.get("/myorder",auth,myOrder);
+router.get("/sellerorder",auth,isAdminOrSeller,sellerOrder);
+router.post("/changeorderstatus",auth,orderStatus);
+
+
 
 
 module.exports = router;
